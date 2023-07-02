855459ad34f48562acf09b28d233b2bba237bee2
andreivg <andreivg595@gmail.com>
2023-07-02 18:49:58 +0200

    Added leaflet map with BCN LatLng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'leaflet';
+  options = {
+    layers: [
+      L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
+        noWrap: true,
+        maxZoom: 18,
+      }),
+    ],
+    zoom: 13,
+    center: L.latLng(41.38879, 2.15899),
+  };
 }
